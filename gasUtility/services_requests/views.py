95afd53336b29a1017584a86fea3c91c0962af07
hemanthu2001 <hemanthu14352892@gmail.com>
2025-02-21 16:15:20 +0530

    created serializers, views and api endpoints

--- gasUtility/services_requests/views.py
+++ gasUtility/services_requests/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .serializers import ServiceRequestSerializer
+from .models import ServiceRequest
 
-# Create your views here.
+
+class ServiceRequestViewSet(viewsets.ModelViewSet):
+    queryset = ServiceRequest.objects.all()
+    serializer_class = ServiceRequestSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return ServiceRequest.objects.all()
+        return ServiceRequest.objects.filter(customer=self.request.user)
+    
+
+    def perform_create(self, serializer):
+        serializer.save(customer=self.request.user)
+
+        
