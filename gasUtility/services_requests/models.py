5786518222edd78683e085762e08c331c529b680
hemanthu2001 <hemanthu14352892@gmail.com>
2025-02-21 15:49:27 +0530

    defined models and migrated

--- gasUtility/services_requests/models.py
+++ gasUtility/services_requests/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ServiceRequest(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('in_progress', 'In Progress'),
+        ('resolved', 'Resolved'),
+    ]
+
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    request_type = models.CharField(max_length=100)
+    description = models.TextField()
+    attachment = models.FileField(upload_to='attachments/' , blank=True, null=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.customer.username} - {self.request_type}'
+    
